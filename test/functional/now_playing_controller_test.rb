a81779ff38987dce0db0cf7355c4c89ef8ea049e
Joao Matos Silva <joao@kspace.pt>
2014-05-09 11:26:45 +0100

    added initial mongo_mapper settings for heroku

--- test/functional/now_playing_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class NowPlayingControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/top_rated_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TopRatedHelperTest < ActionView::TestCase
-end
